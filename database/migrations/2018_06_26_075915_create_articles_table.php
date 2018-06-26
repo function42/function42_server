0816a515daa0c622019800b7d4dd73570f52aa01
function42 <function42@foxmail.com>
2018-06-26 16:54:09 +0800

    新增 Article 表

---
+++ database/migrations/2018_06_26_075915_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('is_public')->default(1);
+            $table->string('title');
+            $table->longText('content');
+            $table->unsignedInteger('user_id');
+            $table->unsignedBigInteger('likes_count')->default(0);
+            $table->unsignedInteger('comments_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
